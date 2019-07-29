dedc45ea9ebc4046accdcfd87373e5c745df094a
Sameer Jain <samir4nov@gmail.com>
2019-07-29 16:53:06 +0530

    about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says Hello!')
+    return HttpResponse('Rango says Hello! <a style="float:right; margin:5px;" href="/rango/about">About</a>')
+
+def about(request):
+    return HttpResponse('You really ought to know about Rango, its awesome! <a style="float:right; margin:5px;" href="/rango/">Home</a>')
