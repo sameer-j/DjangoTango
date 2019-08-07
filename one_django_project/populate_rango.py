300415485af5df4845c62d4ece6502dcb531f13a
Sameer Jain <samir4nov@gmail.com>
2019-08-07 14:28:24 +0530

    Models-templates-views

--- one_django_project/populate_rango.py
+++ one_django_project/populate_rango.py
@@ -22,38 +22,46 @@ def populate():
     python_pages = [
         {
             'title': 'Official Python Tutorial',
-            'url': 'http://docs.python.org/3/tutorial/'
+            'url': 'http://docs.python.org/3/tutorial/',
+            'views': 20
         },
         {
             'title': 'How to Think like a Computer Scientist',
-            'url': 'http://www.greenteapress.com/thinkpython/'
+            'url': 'http://www.greenteapress.com/thinkpython/',
+            'views': 30
         },
         {
             'title': 'Learn Python in 10 Minutes',
-            'url': 'http://www.korokithakis.net/tutorials/python/'
+            'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views': 10
         }
     ]
 
     django_pages = [
         {
             'title': 'Official Django Tutorial',
-            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'
+            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 50
         },
         {
             'title': 'Django Rocks',
-            'url': 'http://www.djangorocks.com/'
+            'url': 'http://www.djangorocks.com/',
+            'views': 5
         },
         {
             'title': 'How to Tango with Django',
-            'url': 'http://www.tangowithdjango.com/'
+            'url': 'http://www.tangowithdjango.com/',
+            'views': 7
         }
     ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+            'views': 25},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'}]
+         'url':'http://flask.pocoo.org',
+            'views': 15}]
     cats = [
         {'name': 'Python', 'pages': python_pages, 'views': 128, 'likes': 64},
         {'name': 'Django', 'pages': django_pages, 'views': 64, 'likes': 32},
@@ -62,7 +70,7 @@ def populate():
     for cat in cats:
         c = add_cat(cat['name'], cat['views'], cat['likes'])
         for p in cat['pages']:
-            add_page(c, p['title'], p['url'])            
+            add_page(c, p['title'], p['url'], p['views'])            
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- one_django_project/rango/views.py
+++ one_django_project/rango/views.py
@@ -1,11 +1,43 @@
-
-from django.http import HttpResponse
+# Rango views
 from django.shortcuts import render
 
+from rango.models import Category, Page
+
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    '''
+    1. Query the database for list of all categories currently stored
+    2. Order the categories in descending order by number of likes
+    3. Retrieve only top 5 -- OR all if less than 5
+    4. pass the list in the context_dict as template context that will be passed
+    to the template engine
+    '''
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
+def show_category(request, category_name_slug):
+    '''
+    Details page for a given category
+    '''
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
+
 def about(request):
+    '''
+    Rango About page
+    '''
     context_dict = {'authorname': 'Sameer Jain'}
     return render(request, 'rango/about.html', context=context_dict)
