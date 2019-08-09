3d86b26a1e39d1ae40dc98202c1e97e5a5cc6e01
Sameer Jain <samir4nov@gmail.com>
2019-08-09 18:52:42 +0530

    add category view

--- one_django_project/rango/views.py
+++ one_django_project/rango/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     '''
@@ -34,6 +35,22 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    '''
+    add category to database from the form
+    '''
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
 
 def about(request):
     '''
